eaba616a3ab5ca75157457489c4e721f2e4098da
Vinicius Dias <carlosv775@gmail.com>
2020-11-28 14:59:08 -0300

    Video 3.1

--- Banco/Conta.hpp
+++ Banco/Conta.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #include <string>
 
-struct Conta
+class Conta
 {
+private:
     std::string numero;
     std::string cpfTitular;
     std::string nomeTitular;
     float saldo;
     
+public:
     void sacar(float valorASacar);
     void depositar(float valorADepositar);
 };
--- Banco/main.cpp
+++ Banco/main.cpp
@@ -7,18 +7,10 @@ using namespace std;
 int main()
 {
     Conta umaConta;
-    umaConta.numero = "123456";
-    umaConta.cpfTitular = "123.456.789-10";
-    umaConta.nomeTitular = "Vinicius";
-    umaConta.saldo = 100;
-
     Conta umaOutraConta;
-    umaOutraConta.saldo = 200;
 
     umaOutraConta.depositar(500);
     umaOutraConta.sacar(200);
-    
-    umaConta.saldo = 1000;
     
     cout << "Uma conta: " << umaConta.saldo << " Outra conta: " << umaOutraConta.saldo << endl;
     
