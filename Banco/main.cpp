21aa999f28a3aef96b2a3c1368ec5b55e3178439
Vinicius Dias <carlosv775@gmail.com>
2020-11-28 17:53:50 -0300

    Video 6.3

--- Banco/main.cpp
+++ Banco/main.cpp
@@ -13,14 +13,14 @@ void ExibeSaldo(const Conta& conta)
 
 int main()
 {
-    Titular titular(string("123.456.789-10"), "Vinicius");
+    Titular titular(Cpf("123.456.789-10"), "Vinicius");
     Conta umaConta("123456", titular);
     umaConta.depositar(500);
     umaConta.sacar(200);
 
     ExibeSaldo(umaConta);
 
-    Titular outro(string("098.765.432-10"), "Dias");
+    Titular outro(Cpf("098.765.432-10"), "Dias");
     Conta umaOutraConta("654321", titular);
     umaOutraConta.depositar(300);
     umaOutraConta.sacar(50);
